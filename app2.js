45b10a5c83d8632e232482bfc1acce1ce0452b9b
wenhuiwu6 <wenhuiwu678@gmail.com>
2025-09-26 12:34:12 -0400

    Update app2.js

--- app2.js
+++ app2.js
@@ -6,11 +6,27 @@
 //     }
 // }
 
-function enter(rideHeightOK, withAdult, healthHold) {
-  if ((rideHeightOK == true || withAdult == true) && !healthHold) {
-    console.log("You are allowed");
-  } else {
-    console.log("You are not allowed");
+// function enter(rideHeightOK, withAdult, healthHold) {
+//   if ((rideHeightOK == true || withAdult == true) && !healthHold) {
+//     console.log("You are allowed");
+//   } else {
+//     console.log("You are not allowed");
+//   }
+// }
+// enter(true, false, false);
+
+// function enter(doorOpen, windowOpen) {
+//   const alarmSilent = !(doorOpen || windowOpen);
+// }
+// enter(true, false, false);
+
+function enter(small, medium, large) {
+  if (
+    (small >= medium && small <= large) ||
+    (small <= medium && small >= large)
+  )
+    console.log("medium is middle");
+  else {
+    console.log("error");
   }
 }
-enter(true, false, false);
