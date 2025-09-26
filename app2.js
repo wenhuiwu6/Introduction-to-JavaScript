d30b1f41738f86c30cdf80eb8f2d8b7354ebd990
wenhuiwu6 <wenhuiwu678@gmail.com>
2025-09-26 12:04:55 -0400

    Update app2.js

--- app2.js
+++ app2.js
@@ -1,9 +1,16 @@
-function enter(hasKey, hasMagic, isBanned){
-    if((hasKey == true || hasMagic == true) && !isBanned) {
-        console.log("you're princess is in another castle");
-    } else {
-        console.log("cannot enter");
-    }
-}
+// function enter(hasKey, hasMagic, isBanned){
+//     if((hasKey == true || hasMagic == true) && !isBanned) {
+//         console.log("you're princess is in another castle");
+//     } else {
+//         console.log("cannot enter");
+//     }
+// }
 
+function enter(rideHeightOK, withAdult, healthHold) {
+  if ((rideHeightOK == true || withAdult == true) && !healthHold) {
+    console.log("You are allowed");
+  } else {
+    console.log("You are not allowed");
+  }
+}
 enter(true, false, false);
