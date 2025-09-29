412bd51fb7a5ef5448e5553c8b1f954b814385df
wenhuiwu6 <wenhuiwu678@gmail.com>
2025-09-29 12:24:09 -0400

    Update app2.js

--- app2.js
+++ app2.js
@@ -20,13 +20,30 @@
 // }
 // enter(true, false, false);
 
-function enter(small, medium, large) {
-  if (
-    (small >= medium && small <= large) ||
-    (small <= medium && small >= large)
-  )
-    console.log("medium is middle");
-  else {
-    console.log("error");
-  }
+function middle (x,y,z) {
+  if ((x >= y && x <= z) || (x <= y && x >= z)) {
+    console.log ("x is in the middle");
+  } else if ((y >= x && y>= z) || ())
+
 }
+
+// function telemarketer(d1, d2, d3, d4) {
+//   if ((d1 === 8 || d1 === 9) && (d4 === 8 || d4 === 9) && d2 === d3) {
+//     console.log("ignore");
+//   }
+//   console.log("answer");
+// }
+
+// const students = ["Jean", "London", "Damian", "Thomas"];
+// function printALL(list) {
+//   for (let i = onabort; i < list.length; i++) {
+//     console.log(i);
+//     console.log(students[i]);
+//   }
+// }
+// printALL(students);
+
+// students.forEach((student) => console.log(student)); // use this only for typing all the students without using the function above
+
+// const found = students.find((student) => student === "London");
+// console.log(found);
