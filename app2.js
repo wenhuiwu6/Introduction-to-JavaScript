1f288ceed2170f03880c875cea38b08a9fc1eb48
wenhuiwu6 <wenhuiwu678@gmail.com>
2025-09-30 12:34:32 -0400

    1

--- app2.js
+++ app2.js
@@ -20,12 +20,12 @@
 // }
 // enter(true, false, false);
 
-function middle (x,y,z) {
-  if ((x >= y && x <= z) || (x <= y && x >= z)) {
-    console.log ("x is in the middle");
-  } else if ((y >= x && y>= z) || ())
+// function middle (x,y,z) {
+//   if ((x >= y && x <= z) || (x <= y && x >= z)) {
+//     console.log ("x is in the middle");
+//   } else if ((y >= x && y>= z) || ())
 
-}
+// }
 
 // function telemarketer(d1, d2, d3, d4) {
 //   if ((d1 === 8 || d1 === 9) && (d4 === 8 || d4 === 9) && d2 === d3) {
@@ -47,3 +47,33 @@ function middle (x,y,z) {
 
 // const found = students.find((student) => student === "London");
 // console.log(found);
+
+const sentence =
+  "The red cat sat on the mat. Why are you so sad cat? Don't ask that.";
+
+function language(sentence) {
+  let t = 0;
+  let s = 0;
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] === "S" || sentence[i] === "s") {
+      s = s + 1;
+    } else if (sentence[i] === "T" || sentence[i] === "t") {
+      t++;
+    }
+  }
+  if (t > s) {
+    console.log("English");
+  } else {
+    console.log("French");
+  }
+}
+language(sentence);
+
+// if (s && S < t && T) {
+//   console.log("Probably English");
+// }
+// if (s && S === t && T) {
+//   console.log("French");
+// } else {
+//   console.log("Probably French");
+// }
