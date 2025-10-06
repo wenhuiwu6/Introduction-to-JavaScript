13e67bf7fd809a3cc304cefa643e78b51f5e911f
wenhuiwu6 <wenhuiwu678@gmail.com>
2025-10-06 12:25:07 -0400

    Update index.js

--- practice/index.js
+++ practice/index.js
@@ -1,12 +1,13 @@
+//select elements
 const button = document.querySelector(".btn");
 const box = document.querySelector(".box");
-
-button.addEvenrListener("click", function (event) {
+//use events
+function makeRGB(event) {
   /* console.log(event);
     console.log(event.target); */
-  let r = Math.floor(Math.random(0, 225));
-  let g = Math.floor(Math.random(0, 225));
-  let b = Math.floor(Math.random(0, 225));
-
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
   box.style.backgoundColor = "rgb(${r},${g},${b}";
-});
+}
+button.addEventListener("click", makeRGB);
